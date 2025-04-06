88e3fec43397778be885bf73548096b8eb6df726
Raynna <andreas.henriksson96@gmail.com>
2025-04-06 20:55:55 +0200

    Added disenchanting at anvil

--- src/main/java/net/raynna/raynnarpg/server/events/ServerPlayerEvents.java
+++ src/main/java/net/raynna/raynnarpg/server/events/ServerPlayerEvents.java
@@ -1,8 +1,14 @@
 package net.raynna.raynnarpg.server.events;
 
 import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.client.gui.components.events.ContainerEventHandler;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
 import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
@@ -16,14 +22,18 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodConstants;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.*;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ContainerScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.AnvilUpdateEvent;
 import net.neoforged.neoforge.event.entity.item.ItemEvent;
-import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
-import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.neoforged.neoforge.event.entity.living.*;
+import net.neoforged.neoforge.event.entity.player.AnvilRepairEvent;
 import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
@@ -229,9 +239,83 @@ public class ServerPlayerEvents {
     public static void onFurnace(PlayerEvent.ItemSmeltedEvent event) {
         if (!(event.getEntity() instanceof ServerPlayer player)) return;
         if (!(event.getEntity().containerMenu instanceof AbstractFurnaceMenu menu)) return;
-
         handleSmeltingEvent(player, menu, event);
     }
+
+
+    @SubscribeEvent
+    public static void onAnvilTake(AnvilRepairEvent event) {
+        System.out.println("RepairEvent");
+        if (event.getEntity().getPersistentData().contains("enchant_removed")) {
+            CompoundTag newItemTag = event.getEntity().getPersistentData().getCompound("enchant_removed");
+            event.getEntity().getPersistentData().remove("enchant_removed");
+            if (event.getEntity().getServer() == null) {
+                return;
+            }
+            HolderLookup.Provider provider = event.getEntity().getServer().registryAccess();
+            Optional<ItemStack> optionalItem = ItemStack.parse(provider, newItemTag);
+            optionalItem.ifPresent(item -> {
+                event.getEntity().getInventory().placeItemBackInInventory(item);
+                System.out.println(item.getHoverName().getString());
+            });
+        }
+    }
+
+    @SubscribeEvent
+    public static void onAnvil(AnvilUpdateEvent event) {
+        ItemStack left = event.getLeft();
+        ItemStack right = event.getRight();
+
+        if (left.isEmpty() || !right.is(Items.BOOK) || !left.isEnchanted()) {
+            return;
+        }
+        ItemEnchantments originalEnchants = left.getTagEnchantments();
+        if (originalEnchants.isEmpty()) {
+            return;
+        }
+
+        Holder<Enchantment> firstEnchantHolder = null;
+        int firstLevel = 0;
+
+        for (Object2IntMap.Entry<Holder<Enchantment>> entry : originalEnchants.entrySet()) {
+            firstEnchantHolder = entry.getKey();
+            firstLevel = entry.getIntValue();
+            break;
+        }
+
+        if (firstEnchantHolder == null) {
+            return;
+        }
+
+        EnchantmentInstance instance = new EnchantmentInstance(firstEnchantHolder, firstLevel);
+        ItemStack enchantedBook = EnchantedBookItem.createForEnchantment(instance);
+
+        Map<Holder<Enchantment>, Integer> mutableEnchants = new HashMap<>();
+        for (Object2IntMap.Entry<Holder<Enchantment>> entry : originalEnchants.entrySet()) {
+            if (!entry.getKey().equals(firstEnchantHolder)) {
+                mutableEnchants.put(entry.getKey(), entry.getIntValue());
+            }
+        }
+
+        ItemStack newItem = left.copy();
+        EnchantmentHelper.setEnchantments(newItem, ItemEnchantments.EMPTY);
+        EnchantmentHelper.updateEnchantments(newItem, mutable -> {
+            for (Map.Entry<Holder<Enchantment>, Integer> entry : mutableEnchants.entrySet()) {
+                mutable.set(entry.getKey(), entry.getValue());
+            }
+        });
+        if (event.getPlayer().getServer() == null) {
+            return;
+        }
+        HolderLookup.Provider provider = event.getPlayer().getServer().registryAccess();
+        CompoundTag compoundTag = new CompoundTag();
+        event.getPlayer().getPersistentData().put("enchant_removed", newItem.save(provider, compoundTag));
+
+        event.setOutput(enchantedBook);
+        event.setCost(1);
+        event.setMaterialCost(1);
+    }
+
 
     @SubscribeEvent
     public static void onCraftEvent(PlayerEvent.ItemCraftedEvent event) {
